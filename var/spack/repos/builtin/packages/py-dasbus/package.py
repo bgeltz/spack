ddf96cbc1252a01fef698feaec723a5260b7b93e
Brad Geltz <brad.geltz@intel.com>
2024-01-03 15:27:43 -0800

    pydasbus: remove extends
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- var/spack/repos/builtin/packages/py-dasbus/package.py
+++ var/spack/repos/builtin/packages/py-dasbus/package.py
@@ -15,8 +15,6 @@ class PyDasbus(PythonPackage):
 
     version("1.7", sha256="a8850d841adfe8ee5f7bb9f82cf449ab9b4950dc0633897071718e0d0036b6f6")
 
-    extends("python")
-
     depends_on("python@3.6:3", type=("build", "run"))
     depends_on("py-setuptools", type="build")
     depends_on("py-pygobject", type=("build", "run"))
