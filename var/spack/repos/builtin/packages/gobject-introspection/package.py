bgeltz/spack

commit acbf0d99c4be4b25815340d650d5f7abacc1f7e1

gobject-introspection: fine-grained glib dependencies, add 1.78.1 (#42222)

var/spack/repos/builtin/packages/gobject-introspection/package.py
@@ -20,6 +20,7 @@ class GobjectIntrospection(MesonPackage, AutotoolsPackage):
 
     license("LGPL-2.0-or-later AND GPL-2.0-or-later AND MIT")
 
+    version("1.78.1", sha256="bd7babd99af7258e76819e45ba4a6bc399608fe762d83fde3cac033c50841bb4")
     version("1.76.1", sha256="196178bf64345501dcdc4d8469b36aa6fe80489354efe71cb7cb8ab82a3738bf")
     version("1.72.1", sha256="012e313186e3186cf0fde6decb57d970adf90e6b1fac5612fe69cbb5ba99543a")
     version("1.72.0", sha256="02fe8e590861d88f83060dd39cda5ccaa60b2da1d21d0f95499301b186beaabc")
@@ -41,9 +42,13 @@ class GobjectIntrospection(MesonPackage, AutotoolsPackage):
     depends_on("sed", when="platform=darwin", type="build")
 
     depends_on("cairo+gobject")
+    depends_on("glib@2.78:", when="@1.78")
+    depends_on("glib@2.76:", when="@1.76")
+    depends_on("glib@2.72:", when="@1.72")
+    depends_on("glib@2.56:", when="@1.56")
     depends_on("glib@2.49.2:", when="@1.49.2:")
-    # version 1.48.0 build fails with glib 2.49.4
     depends_on("glib@2.48.1", when="@1.48.0")
+
     depends_on("libffi")
     # https://gitlab.gnome.org/GNOME/gobject-introspection/-/merge_requests/283
     depends_on("libffi@:3.3", when="@:1.72")  # libffi 3.4 caused seg faults

commit 201b9fda06ad7ad594356177d4adfd37df7e46d9

gobject-introspection: Correct glib versions
- The meson.build requirement that the glib version
  is >= the gobject-introspective version is not in place
  until v1.76.1.
- Prior to that, the requirement was glib >= 2.58.0.
- Bug introduced in acbf0d99c4b, PR #42222.

Signed-off-by: Brad Geltz <brad.geltz@intel.com>

var/spack/repos/builtin/packages/gobject-introspection/package.py
@@ -44,9 +44,9 @@ class GobjectIntrospection(MesonPackage, AutotoolsPackage):
     depends_on("cairo+gobject")
     depends_on("glib@2.78:", when="@1.78")
     depends_on("glib@2.76:", when="@1.76")
-    depends_on("glib@2.72:", when="@1.72")
+    depends_on("glib@2.58:", when="@1.72")
     depends_on("glib@2.56:", when="@1.56")
-    depends_on("glib@2.49.2:", when="@1.49.2:")
+    depends_on("glib@2.49.2:", when="@1.49.2")
     depends_on("glib@2.48.1", when="@1.48.0")
 
     depends_on("libffi")