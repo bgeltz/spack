c5b098f15d79f4c28280ec62a773ee722330074b
Brad Geltz <brad.geltz@intel.com>
2025-07-28 14:41:37 -0700

    py_setuptools_scm: Add v6.4.2
    Signed-off-by: Brad Geltz <brad.geltz@intel.com>

--- var/spack/repos/builtin/packages/py-setuptools-scm/package.py
+++ var/spack/repos/builtin/packages/py-setuptools-scm/package.py
@@ -19,6 +19,7 @@ class PySetuptoolsScm(PythonPackage):
     version("7.1.0", sha256="6c508345a771aad7d56ebff0e70628bf2b0ec7573762be9960214730de278f27")
     version("7.0.5", sha256="031e13af771d6f892b941adb6ea04545bbf91ebc5ce68c78aaf3fff6e1fb4844")
     version("7.0.3", sha256="cf8ab8e235bed840cd4559b658af0d8e8a70896a191bbc510ee914ec5325332d")
+    version("6.4.2", sha256="6833ac65c6ed9711a4d5d2266f8024cfa07c533a0e55f4c12f6eff280a5a9e30")
     version("6.3.2", sha256="a49aa8081eeb3514eb9728fa5040f2eaa962d6c6f4ec9c32f6c1fba88f88a0f2")
     version("6.0.1", sha256="d1925a69cb07e9b29416a275b9fadb009a23c148ace905b2fb220649a6c18e92")
     version("5.0.2", sha256="83a0cedd3449e3946307811a4c7b9d89c4b5fd464a2fb5eeccd0a5bb158ae5c8")
